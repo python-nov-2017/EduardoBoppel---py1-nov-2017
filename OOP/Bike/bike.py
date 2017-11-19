0cc25ec1af30093498539d3a1765718fd26b948b
eboppel <eboppel@gmail.com>
2017-11-19 13:23:12 -0600

    Added OOP Bike Exercise

---
+++ OOP/Bike/bike.py
@@ -0,0 +1,31 @@
+class Bike(object):
+    def __init__(self, price, max_speed):
+        self.price = price
+        self.max_speed = max_speed
+        self.miles = 0
+
+    def displayInfo(self):
+        print self.price, self.max_speed, self.miles
+        return self
+
+    def ride(self):
+        print "riding"
+        self.miles += 10
+        return self
+
+    def reverse(self):
+        if self.miles > 0:
+            print "reversing"
+            self.miles -= 5
+        else:
+            print "cannot reverse any more"
+        return self
+
+
+bike1 = Bike(175, "18mph")
+bike2 = Bike(200, "25mph")
+bike3 = Bike(250, "32mph")
+
+bike1.ride().ride().ride().displayInfo()
+bike2.ride().ride().reverse().reverse().displayInfo()
+bike3.reverse().reverse().reverse().displayInfo()

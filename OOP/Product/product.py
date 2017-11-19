d104be7a740cb9af15a2a7c6227e1887700ca985
eboppel <eboppel@gmail.com>
2017-11-19 14:03:11 -0600

    Added OOP Product Exercise

---
+++ OOP/Product/product.py
@@ -0,0 +1,53 @@
+class Product(object):
+    def __init__(self, price, name, weight, brand):
+        self.price = price
+        self.name = name
+        self.weight = weight
+        self.brand = brand
+        self.status = "For Sale"
+
+    def sell(self):
+        self.status = "Sold"
+        return self
+
+    def add_tax(self, tax):
+        self.price *= (1+tax)
+        return self.price
+
+    def return_item(self, reason):
+        if reason == "defective":
+            self.status = "defective"
+            self.price = 0
+        elif reason == "new":
+            self.status = "For Sale"
+        elif reason == "opened":
+            self.status = "Used"
+            self.price *= 0.80
+        else:
+            print "Select a valid return reason"
+        return self
+
+    def display_info(self):
+        print "Price: ", self.price
+        print "Name: ", self.name
+        print "Weight: ", self.weight
+        print "Brand: ", self.brand
+        print "Status: ", self.status
+        return self
+
+    
+
+product1 = Product(15, "Hammer", "15kg", "Brand A")
+product2 = Product(12, "Nail", "5kg", "Brand B")
+product3 = Product(17, "Saw", "10kg", "Brand C")
+
+
+product1.display_info()
+product1.add_tax(0.12)
+product1.sell().display_info()
+product1.return_item("opened").display_info()
+
+product2.display_info()
+product3.display_info()
+
+

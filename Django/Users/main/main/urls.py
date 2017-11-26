a9e31e29587224a596d7827f7d7dae62710c6d62
eboppel <eboppel@gmail.com>
2017-11-26 14:56:42 -0600

    Added Django NinjaDojo Exercise

--- Django/Users/main/main/urls.py
+++ Django/Users/main/main/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('apps.user_login.urls')),
+    url(r'^ninjas/', include('apps.dojo_ninjas.urls')),
     url(r'^admin/', admin.site.urls),
 ]
